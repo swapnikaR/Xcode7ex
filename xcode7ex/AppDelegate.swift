3ec1aa58f4410fecd0988c53d422d86d5666cc48
Swapnika4 <swapnika.rebali@gmail.com>
2017-02-08 15:30:38 -0800

    Initial Commit

---
+++ xcode7ex/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xcode7ex
+//
+//  Created by Swapnika Rebali on 2/8/17.
+//  Copyright © 2017 Swapnika Rebali. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xcode7ex/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xcode7ex
+//
+//  Created by Swapnika Rebali on 2/8/17.
+//  Copyright © 2017 Swapnika Rebali. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
